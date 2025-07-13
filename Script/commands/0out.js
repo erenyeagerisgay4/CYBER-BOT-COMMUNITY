237df9e3aa1e5b8c05cd489c37041f3215e6a18c
erenyeagerisgay4 <mollarifat384@gmail.com>
2025-07-13 20:15:43 +0600

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "🦋𝐒𝐨𝐜𝐢𝐞𝐭𝐲 𝐎𝐟 𝐀𝐧𝐢𝐦𝐞 𝐋𝐨𝐯𝐞𝐫𝐬🎊🎭",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
