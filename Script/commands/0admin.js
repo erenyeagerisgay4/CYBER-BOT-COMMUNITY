85ad5e66138d6b32df0658e066e8fc1c22f77cd0
erenyeagerisgay4 <mollarifat384@gmail.com>
2025-07-13 19:41:47 +0600

    Update 0admin.js

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "0admin",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "🦋𝐒𝐨𝐜𝐢𝐞𝐭𝐲 𝐎𝐟 𝐀𝐧𝐢𝐦𝐞 𝐋𝐨𝐯𝐞𝐫𝐬🎊🎭",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
