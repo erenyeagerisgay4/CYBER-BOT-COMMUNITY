f4bfa3e4da0b070d3aeef540804ff83ab40d63ac
erenyeagerisgay4 <mollarifat384@gmail.com>
2025-07-13 20:15:28 +0600

    Update bro.js

--- Script/commands/bro.js
+++ Script/commands/bro.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "brother",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
+  credits: "🦋𝐒𝐨𝐜𝐢𝐞𝐭𝐲 𝐎𝐟 𝐀𝐧𝐢𝐦𝐞 𝐋𝐨𝐯𝐞𝐫𝐬🎊🎭", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
