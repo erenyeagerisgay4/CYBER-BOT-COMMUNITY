fd178e8eb845d5adf4b0e59ff15a1da02baa1d6a
erenyeagerisgay4 <mollarifat384@gmail.com>
2025-07-13 19:16:12 +0600

    Update kanna.js

--- Script/commands/kanna.js
+++ Script/commands/kanna.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "kanna",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "🦋𝐒𝐨𝐜𝐢𝐞𝐭𝐲 𝐎𝐟 𝐀𝐧𝐢𝐦𝐞 𝐋𝐨𝐯𝐞𝐫𝐬🎊🎭",
   description: "See pictures of baby dragons",
   commandCategory: "ramdom-images",
   usages: "kanna",
