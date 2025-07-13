bac5c20aad33eb5c501f4575e607a06d3f652fc3
erenyeagerisgay4 <mollarifat384@gmail.com>
2025-07-13 21:21:27 +0600

    Update married.js

--- Script/commands/married.js
+++ Script/commands/married.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "married",
     version: "3.1.1",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "🦋𝐒𝐨𝐜𝐢𝐞𝐭𝐲 𝐎𝐟 𝐀𝐧𝐢𝐦𝐞 𝐋𝐨𝐯𝐞𝐫𝐬🎊🎭",
     description: "married",
     commandCategory: "img",
     usages: "[@mention]",
